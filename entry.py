3b2042473a367ae1716893be74ee41fcdf7bd856
Prakash <geekyprakash1@gmail.com>
2025-06-09 02:09:55 +0530

    Update entry.py

--- entry.py
+++ entry.py
@@ -93,7 +93,7 @@ def init_session_state():
         if key not in st.session_state:
             st.session_state[key] = default_value
 
-st.set_page_config(page_title="PPH CRM", layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(page_title="PPH CRM 1", layout="wide", initial_sidebar_state="expanded")
 init_session_state()
 
 # Helper functions
